346f0dbac5ca5c50c24a0c601149b6b7b807cd9d
Hillgrove <github@hillgrove.dk>
2023-03-16 11:17:12 +0000

    Thu, Mar 16, 2023, 12:17 PM +01:00

--- lab1/population/population.c
+++ lab1/population/population.c
@@ -1,36 +1,45 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_start(void);
+int get_end(void);
+int num_years (int start, int end);
+
 int main(void)
 {
-    int get_start(void)
-    {
-        int start_size;
-        do
-        {
-            start_size = get_int("Start size: ");
-        }
-        while (start_size < 9);
 
-        return start_size
-    }
-
-    int get_end(void)
-    {
-        int end_size;
-        do
-        {
-            end_size = get_int("End Size: ");
-        }
-        while (start_size > end_size);
-
-        return end_size
-    }
-
-    int num_years (start, end)
 
 
         // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
+}
+
+int get_start(void)
+{
+    int start_size;
+    do
+    {
+        start_size = get_int("Start size: ");
+    }
+    while (start_size < 9);
+
+    return start_size;
+}
+
+int get_end(void)
+{
+    int end_size;
+    do
+    {
+        end_size = get_int("End Size: ");
+    }
+    while (start_size > end_size);
+
+    return end_size
+}
+
+int num_years (start, end)
+{
+    return 0;
 }
