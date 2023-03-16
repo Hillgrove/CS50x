01658bfd34bbdbb5b06b18ec684074d287ff6c6f
Hillgrove <github@hillgrove.dk>
2023-03-16 09:50:46 +0000

    Thu, Mar 16, 2023, 10:50 AM +01:00

--- lab1/prime/prime.c
+++ lab1/prime/prime.c
@@ -41,7 +41,7 @@ bool prime(int number)
     else
     {
         int count = 0;
-        for (int i = 2; i <= number / 2; i++)
+        for (int i = 2; i <= number; i++)
         {
             if (number % i == 0)
             {
