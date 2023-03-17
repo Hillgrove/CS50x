d691a1c8d114f208d0508886e9aaef54ee459215
Hillgrove <github@hillgrove.dk>
2023-03-17 11:35:14 +0000

    Fri, Mar 17, 2023, 12:35 PM +01:00

--- week01/pset01/credit/credit.c
+++ week01/pset01/credit/credit.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // American Express uses 15-digit numbers
+    // MasterCard uses 16-digit numbers
+    // Visa uses 13- and 16-digit numbers
 }
