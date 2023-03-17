b6d6822d83cedb5316b0348c97a85d78a5ef7fe8
Hillgrove <github@hillgrove.dk>
2023-03-17 11:38:04 +0000

    Fri, Mar 17, 2023, 12:38 PM +01:00

--- week01/pset01/credit/credit.c
+++ week01/pset01/credit/credit.c
@@ -3,10 +3,17 @@
 
 int main(void)
 {
-    // American Express uses 15-digit numbers
-    // All American Express numbers start with 34 or 37
-    // MasterCard uses 16-digit numbers
-    // most MasterCard numbers start with 51, 52, 53, 54, or 55
-    // Visa uses 13- and 16-digit numbers
-    // all Visa numbers start with 4
+    /*
+    American Express uses 15-digit numbers
+    All American Express numbers start with 34 or 37
+    MasterCard uses 16-digit numbers
+    most MasterCard numbers start with 51, 52, 53, 54, or 55
+    Visa uses 13- and 16-digit numbers
+    all Visa numbers start with 4
+
+    Luhn’s Algorithm
+    1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+    2. Add the sum to the sum of the digits that weren’t multiplied by 2.
+    3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+    */
 }
