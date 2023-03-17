e2fefa26e250c9f14a5155db432be9cd8a0b121d
Hillgrove <github@hillgrove.dk>
2023-03-17 11:36:30 +0000

    Fri, Mar 17, 2023, 12:36 PM +01:00

--- week01/pset01/credit/credit.c
+++ week01/pset01/credit/credit.c
@@ -4,6 +4,9 @@
 int main(void)
 {
     // American Express uses 15-digit numbers
+    // All American Express numbers start with 34 or 37
     // MasterCard uses 16-digit numbers
+    // most MasterCard numbers start with 51, 52, 53, 54, or 55
     // Visa uses 13- and 16-digit numbers
+    // all Visa numbers start with 4
 }
