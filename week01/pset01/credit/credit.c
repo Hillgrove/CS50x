ad56f5f46bb7c5611bd83c1094ea7eea3afcdae8
Hillgrove <github@hillgrove.dk>
2023-05-10 14:03:06 +0000

    Wed, May 10, 2023, 4:03 PM +02:00

--- week01/pset01/credit/credit.c
+++ week01/pset01/credit/credit.c
@@ -57,8 +57,8 @@ string check_cardnumber(long cardnumber)
 bool valid_checksum(long cardnumber)
 {
     int digit;
-    int length = get_length(cardnumber); // 3 78 28 22 46 31 00 05   =>   0 0 3 4 2 2 7 0
-    printf("Card length = %i\n", length);
+    int length = get_length(cardnumber);
+    printf("Card length = %i\n", length); // delete when code done
 
     for (int i = 0; i <= length; i+=1)
     {
@@ -66,7 +66,7 @@ bool valid_checksum(long cardnumber)
         if (i % 2 == 0)
         {
             digit = cardnumber % 10;
-            printf("Digit %i: %i\n", i, digit); // for testing of loop
+            printf("Digit %i: %i\n", i, digit); // for testing of loop | 378282246310005   =>   0 0 3 4 2 2 7 0
         }
 
 
