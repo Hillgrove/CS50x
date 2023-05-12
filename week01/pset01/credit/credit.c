00a411ebd61ff15c3ad00f30ad6a0f3f14954864
Hillgrove <github@hillgrove.dk>
2023-05-12 12:03:04 +0000

    Fri, May 12, 2023, 2:03 PM +02:00

--- week01/pset01/credit/credit.c
+++ week01/pset01/credit/credit.c
@@ -74,7 +74,8 @@ int calculate_checksum(long cardnumber)
 
             while (digit > 0)
             {
-                checksum += digit % 10
+                checksum += digit % 10;
+                digit = digit / n;
             }
 
 
