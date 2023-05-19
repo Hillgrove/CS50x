b306ef6c8942bcda7260b9a42d642b33a8763f5a
Hillgrove <github@hillgrove.dk>
2023-05-19 22:28:48 +0000

    Sat, May 20, 2023, 12:28 AM +02:00

--- week02/pset02/bulbs/test.c
+++ week02/pset02/bulbs/test.c
@@ -1,5 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
 int main(void)
 {
-    int array1 = {1, 2, 3, 4, 5, 6, 7, 8}
-    printf("%i", array1)
+    int array1[8];
+    for ( int i = 0; i < 8; i++)
+    {
+        printf("%i", array1[i]);
+    }
+    printf("\n");
 }
