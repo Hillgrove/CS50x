0f103697b615e206f3631e87b225e5e1d88696e4
Hillgrove <github@hillgrove.dk>
2023-05-19 18:41:38 +0000

    Fri, May 19, 2023, 8:41 PM +02:00

--- week02/pset02/bulbs/bulbs.c
+++ week02/pset02/bulbs/bulbs.c
@@ -21,7 +21,12 @@ void print_bulb(int bit);
 
 int main(void)
 {
-    // TODO
+    string message = getstring("Message: ");
+
+    
+    // TODO: convert the given string into a series of 8-bit binary numbers, one for each character of the string.
+    // TODO: use the provided print_bulb function to print a series of 0s and 1s as a series of yellow and black emoji, which represent on and off light bulbs.
+    // TODO: Each “byte” of 8 symbols should be printed on its own line when outputted; there should be a \n after the last “byte” of 8 symbols as well.
 }
 
 void print_bulb(int bit)
