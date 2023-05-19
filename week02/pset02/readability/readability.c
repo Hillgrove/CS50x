a09e2a3bb246fc7519fd041c466124aac0ed6158
Hillgrove <github@hillgrove.dk>
2023-05-19 11:33:34 +0000

    Fri, May 19, 2023, 1:33 PM +02:00

--- week02/pset02/readability/readability.c
+++ week02/pset02/readability/readability.c
@@ -1,9 +1,10 @@
 /*
 For this problem, you’ll implement a program that calculates the approximate grade level needed to comprehend some text, per the below.
 
-Using the Coleman-Liau index. The Coleman-Liau index of a text is designed to output that (U.S.) grade level that is needed to understand some text. The formula is
+The Coleman-Liau index of a text is designed to output that (U.S.) grade level that is needed to understand some text. The formula is
 
 index = 0.0588 * L - 0.296 * S - 15.8
+
 where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
 */
 
@@ -12,5 +13,5 @@ where L is the average number of letters per 100 words in the text, and S is the
 
 int main(void)
 {
-
+    
 }
