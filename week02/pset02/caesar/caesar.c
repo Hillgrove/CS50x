cce2ef21a11e2a94d6af560293466e6093310cac
Hillgrove <github@hillgrove.dk>
2023-05-22 19:09:46 +0000

    Mon, May 22, 2023, 9:09 PM +02:00

--- week02/pset02/caesar/caesar.c
+++ week02/pset02/caesar/caesar.c
@@ -9,12 +9,14 @@ bool is_number(string text);
 
 int main(int argc, string argv[])
 {
+    // Throw error if more than 1 argument was used
     if (argc != 2)
     {
         printf("Command needs a single numerical argument\n");
         return 1;
     }
 
+    // Check if argument is a number
     else if (!is_number(argv[1]))
     {
         printf("Usage: ./caesar key\n");
@@ -22,7 +24,10 @@ int main(int argc, string argv[])
     }
     else
     {
-
+        // TODO: Convert argument string to int
+        // TODO: Ask user for message
+        // TODO: Loop each char in message
+            // TODO: If letter encode char
     }
 }
 
