92a6ec59d8f20ca053977092a9613a92cb41b02c
Hillgrove <github@hillgrove.dk>
2023-05-22 11:47:33 +0000

    Mon, May 22, 2023, 1:47 PM +02:00

--- week02/pset02/caesar/caesar.c
+++ week02/pset02/caesar/caesar.c
@@ -4,7 +4,10 @@
 
 int main(int argc, string argv[])
 {
-    if (argc != 2 && isargv[0])
+    if (argc != 2 && isalpha(argv[0]))
+    {
+        printf("Usage: ./caesar key");
+    }
 
     printf("argc: %i\n", argc);
     printf("argv: %s\n", argv[0]);
