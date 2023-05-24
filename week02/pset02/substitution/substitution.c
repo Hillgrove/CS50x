c74959b410f3223d093802da5d0c9cfa4a85c802
Hillgrove <github@hillgrove.dk>
2023-05-24 20:28:23 +0000

    Wed, May 24, 2023, 10:28 PM +02:00

--- week02/pset02/substitution/substitution.c
+++ week02/pset02/substitution/substitution.c
@@ -34,7 +34,7 @@ int main(int argc, string argv[])
     }
 
     // If everything is correct, ask for plaintext
-    string plaintext = get_string("Plaintext:");
+    string plaintext = get_string("Plaintext: ");
 
     // encipher the plaintext and print it
     string ciphertext = encipher(plaintext, key);
