ad85f8bcc6ec36e4db2496b5b115841ffa496d74
Hillgrove <github@hillgrove.dk>
2023-05-24 14:06:48 +0000

    Wed, May 24, 2023, 4:06 PM +02:00

--- week02/pset02/substitution/substitution.c
+++ week02/pset02/substitution/substitution.c
@@ -15,6 +15,14 @@ bool no_repeats(string key);
 
 int main(int argc, string argv[])
 {
+    if (argc != 2)
+    {
+        
+    }
+    else
+    {
+
+    }
     // Get key
     string key = argv[1];
 
