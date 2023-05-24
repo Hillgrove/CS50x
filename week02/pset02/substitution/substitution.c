bf0375d322d075d799e816071871847c2ae7d8f3
Hillgrove <github@hillgrove.dk>
2023-05-24 13:48:47 +0000

    Wed, May 24, 2023, 3:48 PM +02:00

--- week02/pset02/substitution/substitution.c
+++ week02/pset02/substitution/substitution.c
@@ -7,5 +7,17 @@ A program that implements a substitution cipher
 
 int main(int argc, string argv[])
 {
+    // TODO: Get key
+    // TODO: Validate key
+        // TODO: check key length
+        // TODO: check for non-alphabetic characters
+        // TODO: check for repeated characters (case-insensitive)
+    // TODO: Get plaintext
+    // TODO: Encipher
+        // TODO: for each alphabetic character, determine what letter it maps to
+        // TODO: preserve case
+        // TODO: leave non-alphabetic charaters as-is
+    // TODO: Print ciphertext
+
 
 }
