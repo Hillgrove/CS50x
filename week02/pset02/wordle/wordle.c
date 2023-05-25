71dee7c40469bb46f46f57c664f0cee9ca26e701
Hillgrove <github@hillgrove.dk>
2023-05-25 11:54:01 +0000

    Thu, May 25, 2023, 1:54 PM +02:00

--- week02/pset02/wordle/wordle.c
+++ week02/pset02/wordle/wordle.c
@@ -41,7 +41,7 @@ int main(int argc, string argv[])
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    if (isdigit(argv[1]))
+    if (53 >= argv[1] >= 56)
     {
         printf("Error: wordsize must be either 5, 6, 7, or 8\n");
         return 1;
