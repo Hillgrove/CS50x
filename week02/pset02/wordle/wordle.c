51576e00fb041b9bdc3957c34119593872c1f337
Hillgrove <github@hillgrove.dk>
2023-05-25 13:39:08 +0000

    Thu, May 25, 2023, 3:39 PM +02:00

--- week02/pset02/wordle/wordle.c
+++ week02/pset02/wordle/wordle.c
@@ -90,7 +90,11 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
-        
+        for (int i = 0; i < wordsize; i++)
+        {
+            status[i] = WRONG;
+        }
+
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
