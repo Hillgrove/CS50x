a02614bd88eccf4e38d0d2642b7158aa723141bd
Hillgrove <github@hillgrove.dk>
2023-05-25 11:38:57 +0000

    Thu, May 25, 2023, 1:38 PM +02:00

--- week02/pset02/wordle/wordle.c
+++ week02/pset02/wordle/wordle.c
@@ -40,6 +40,15 @@ int main(int argc, string argv[])
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
+    if (5 > (int) argv[1] > 8)
+    {
+        printf("Error: wordsize must be either 5, 6, 7, or 8\n");
+        return 1;
+    }
+    else
+    {
+        wordsize = argv[1];
+    }
 
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
