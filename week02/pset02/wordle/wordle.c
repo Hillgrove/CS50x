6a0a273cd247437d54b2ace1c486c7fdef0ac1f8
Hillgrove <github@hillgrove.dk>
2023-05-26 12:23:03 +0000

    Fri, May 26, 2023, 2:23 PM +02:00

--- week02/pset02/wordle/wordle.c
+++ week02/pset02/wordle/wordle.c
@@ -138,12 +138,6 @@ int check_word(string guess, int wordsize, int status[], string choice)
     int score = 0;
     choice = "chair";
 
-    for (int i = 0; i < wordsize; i++)
-        {
-            printf("%i ", status[i]);
-        }
-    printf("\n");
-
     // compare guess to choice and score points as appropriate, storing points in status
     // TODO #5
 
@@ -151,6 +145,7 @@ int check_word(string guess, int wordsize, int status[], string choice)
     // iterate over each letter of the guess
     for (int i = 0; i < wordsize; i++)
     {
+        score = 0;
         // iterate over each letter of the choice
         for (int j = 0; j < wordsize; j++)
         {
