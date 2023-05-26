dc35a240ece457fc5e3a05a37e5bfdb69688f586
Hillgrove <github@hillgrove.dk>
2023-05-26 13:30:47 +0000

    Fri, May 26, 2023, 3:30 PM +02:00

--- week02/pset02/wordle/wordle.c
+++ week02/pset02/wordle/wordle.c
@@ -176,7 +176,11 @@ void print_word(string guess, int wordsize, int status[])
 {
     // print word character-for-character with correct color coding, then reset terminal font to normal
     // TODO #6
-    
+    for (int i = 0; i < wordsize; i++)
+    {
+        string color = 
+        printf("",);
+    }
 
     printf("\n");
     return;
