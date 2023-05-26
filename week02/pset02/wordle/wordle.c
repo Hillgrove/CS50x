9eae59ba2d4a7b61881162676e41c4a2a55c220d
Hillgrove <github@hillgrove.dk>
2023-05-26 11:42:04 +0000

    Fri, May 26, 2023, 1:42 PM +02:00

--- week02/pset02/wordle/wordle.c
+++ week02/pset02/wordle/wordle.c
@@ -141,11 +141,22 @@ int check_word(string guess, int wordsize, int status[], string choice)
 
     // HINTS
     // iterate over each letter of the guess
-    
+    for (int i = 0; i < wordsize; i++)
+    {
         // iterate over each letter of the choice
+        for (int j = 0; j < wordsize; j++)
+        {
             // compare the current guess letter to the current choice letter
                 // if they're the same position in the word, score EXACT points (green) and break so you don't compare that letter further
                 // if it's in the word, but not the right spot, score CLOSE point (yellow)
+            if (guess[i] )
+        }
+
+    }
+
+
+
+
         // keep track of the total score by adding each individual letter's score from above
 
     return score;
