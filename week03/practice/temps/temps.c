52c4bd0e400734d4e9cafc5a415e22469c8b09c4
Hillgrove <github@hillgrove.dk>
2023-05-28 14:29:06 +0000

    Sun, May 28, 2023, 4:29 PM +02:00

--- week03/practice/temps/temps.c
+++ week03/practice/temps/temps.c
@@ -78,14 +78,15 @@ void sort_cities(void)
     for (int i = 0; i < NUM_CITIES; i++)
     {
         index_lowest = i;
-        printf("Comparing %s: %i with ", temps[i].city, temps[i].temp);
+
 
         for (int j = i + 1; j < NUM_CITIES - (i + 1); j++)
         {
-            printf("%s: %i\n", temps[i].city, temps[i].temp);
+            printf("Comparing %s: %i with %s: %i\n", temps[i].city, temps[i].temp,  temps[j].city, temps[j].temp);
             if (temps[j].temp < temps[i].temp)
             {
                 index_lowest = j;
+                printf("\nFound lower temp with %s: %i\n", temps[j].city, temps[j].temp);
             }
         }
 
