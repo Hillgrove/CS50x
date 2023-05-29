e8fae59c8de0c9d0840f6c7f0716a0928cd9df41
Hillgrove <github@hillgrove.dk>
2023-05-29 11:57:03 +0000

    Mon, May 29, 2023, 1:57 PM +02:00

--- week03/practice/snackbar/snackbar.c
+++ week03/practice/snackbar/snackbar.c
@@ -114,7 +114,8 @@ float get_cost(string item)
     {
         printf("i: %i - Comparing '%s' with '%s'\n", i, item, menu[i].item);
 
-        if (strcasecmp(menu[i].item, item) = )
+        // compare 2 strings (case insensitive)
+        if (strcasecmp(menu[i].item, item) = 0)
         {
             printf("returning price of %s: %f", menu[i].item, menu[i].price);
             return menu[i].price;
