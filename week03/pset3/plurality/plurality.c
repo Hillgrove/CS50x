7417658e9334aefc70dfc4bb0fb076ef6faf85dd
Hillgrove <github@hillgrove.dk>
2023-05-29 14:38:10 +0000

    Mon, May 29, 2023, 4:38 PM +02:00

--- week03/pset3/plurality/plurality.c
+++ week03/pset3/plurality/plurality.c
@@ -94,9 +94,9 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidate[i].votes == highest_vote)
+        if (candidates[i].votes == highest_vote)
         {
-            printf("%s", candidate[i].name);
+            printf("%s", candidates[i].name);
         }
     }
 
