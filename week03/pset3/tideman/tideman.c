eb7a1e8e45e061e991a43483893d6f3cfaaa9adc
Hillgrove <github@hillgrove.dk>
2023-06-08 22:47:11 +0000

    Fri, Jun 9, 2023, 12:47 AM +02:00

--- week03/pset3/tideman/tideman.c
+++ week03/pset3/tideman/tideman.c
@@ -174,10 +174,8 @@ void sort_pairs(void)
 
         for (int j = i + 1; j < pair_count; j++)
         {
-            int max_margin = candidates[pairs[idx_max].winner, b] - [b, a];
-
-            int max_margin = pairs[idx_max].winner - pairs[idx_max].loser;
-            int j_margin = pairs[j].winner - pairs[j].loser;
+            int max_margin = candidates[pairs[idx_max].winner][pairs[idx_max].loser] - candidates[pairs[idx_max].loser][pairs[idx_max].winner];
+            int j_margin = candidates[][]   pairs[j].winner - pairs[j].loser;
 
             if (j_margin > max_margin)
             {
