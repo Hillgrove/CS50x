240f43e7f6c399552d9c72be43e4323ea9a1bc5c
Hillgrove <github@hillgrove.dk>
2023-06-08 20:26:29 +0000

    Thu, Jun 8, 2023, 10:26 PM +02:00

--- week03/pset3/tideman/tideman.c
+++ week03/pset3/tideman/tideman.c
@@ -141,7 +141,7 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    printf("checking pairs in preferences array:\n");
+    printf("\nchecking pairs in preferences array:\n");
     for (int i = 0; i < candidate_count - 1; i++)
     {
         for (int j = i + 1; j < candidate_count; j++)
