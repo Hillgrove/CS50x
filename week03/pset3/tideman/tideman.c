2a6e956818b9e26e56b2669db82d751bfae3601b
Hillgrove <github@hillgrove.dk>
2023-06-14 09:54:46 +0000

    Wed, Jun 14, 2023, 11:54 AM +02:00

--- week03/pset3/tideman/tideman.c
+++ week03/pset3/tideman/tideman.c
@@ -168,7 +168,7 @@ void sort_pairs(void)
 {
     int i, j;
     bool swapped;
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < pair_count - 1; i++) {
         swapped = false;
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
