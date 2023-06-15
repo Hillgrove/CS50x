edd7fc86d0793679df3e0eadb39f48f2012fcc27
Hillgrove <github@hillgrove.dk>
2023-06-15 14:35:20 +0000

    Thu, Jun 15, 2023, 4:35 PM +02:00

--- week03/pset3/tideman/tideman.c
+++ week03/pset3/tideman/tideman.c
@@ -231,7 +231,7 @@ bool loop(int winner, int loser)
     {
         if (locked[i][j] == 1)
         {
-
+            // if something some to stop loop
             loop(i, j); // to check if j has a loop to test if => and ultimately if it creates a loop
         }
     }
