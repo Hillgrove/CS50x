1b19c7ef8108c0990d38f69dc6c6b3fdaad2534f
Hillgrove <github@hillgrove.dk>
2023-06-15 10:31:44 +0000

    Thu, Jun 15, 2023, 12:31 PM +02:00

--- week03/pset3/tideman/tideman.c
+++ week03/pset3/tideman/tideman.c
@@ -194,10 +194,12 @@ void sort_pairs(void)
             // Swap the found maximum element with the first element
             if(max_idx != i)
             {
-                printf("\nSwapping pair (%i, %i) with (%i, %i)\n");
+                printf("\nSwapping pair (%i, %i) with (%i, %i)\n", pairs[max_idx].winner,pairs[max_idx].loser, pairs[j].winner,pairs[j].loser);
                 pair temp = pairs[max_idx];
                 pairs[max_idx] = pairs[i];
                 pairs[i] = temp;
+
+                print_pairs();
             }
         }
     }
