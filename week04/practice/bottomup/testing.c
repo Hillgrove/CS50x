d32731e4686dfab59623df80deda835d15c89427
Hillgrove <github@hillgrove.dk>
2023-06-17 15:03:43 +0000

    Sat, Jun 17, 2023, 5:03 PM +02:00

--- week04/practice/bottomup/testing.c
+++ week04/practice/bottomup/testing.c
@@ -38,7 +38,6 @@ int main(int argc, char *argv[])
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
-        fclose(outptr);
         fclose(inptr);
         printf("Unsupported file format.\n");
         return 4;
@@ -46,47 +45,11 @@ int main(int argc, char *argv[])
 
     bi.biHeight = abs(bi.biHeight);
 
-    // Write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
     // Write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
-    // Determine padding for scanlines
-    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
-    // Iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
-    {
-        // Iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
-        {
-            // Temporary storage
-            RGBTRIPLE triple;
-
-            // Read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
-
-            // Write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-        }
-
-        // Skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
-
-        // Then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
-        }
-    }
-
-    // Close infile
+        // Close infile
     fclose(inptr);
-
-    // Close outfile
-    fclose(outptr);
 
     // Success
     return 0;
