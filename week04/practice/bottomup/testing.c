8ae1db8bcc039e90a52330d36c7a287616ecb3cf
Hillgrove <github@hillgrove.dk>
2023-06-17 15:05:39 +0000

    Sat, Jun 17, 2023, 5:05 PM +02:00

--- week04/practice/bottomup/testing.c
+++ week04/practice/bottomup/testing.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
     // Ensure proper usage
-    if (argc != 2)
+    if (argc != 3)
     {
         printf("Usage: copy infile outfile\n");
         return 1;
@@ -16,15 +16,24 @@ int main(int argc, char *argv[])
 
     // Remember filenames
     char *infile = argv[1];
+    char *outfile = argv[2];
 
     // Open input file
-    FILE *inptr = fopen(infile, "r+");
+    FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
         printf("Could not open %s.\n", infile);
         return 2;
     }
 
+    // Open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        printf("Could not create %s.\n", outfile);
+        return 3;
+    }
 
     // Read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
@@ -38,6 +47,7 @@ int main(int argc, char *argv[])
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
+        fclose(outptr);
         fclose(inptr);
         printf("Unsupported file format.\n");
         return 4;
@@ -45,11 +55,47 @@ int main(int argc, char *argv[])
 
     bi.biHeight = abs(bi.biHeight);
 
-    // Write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    // Write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
-        // Close infile
+    // Write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // Determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        // Iterate over pixels in scanline
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+            // Temporary storage
+            RGBTRIPLE triple;
+
+            // Read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+
+            // Write RGB triple to outfile
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+        }
+
+        // Skip over padding, if any
+        fseek(inptr, padding, SEEK_CUR);
+
+        // Then add it back (to demonstrate how)
+        for (int k = 0; k < padding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+    }
+
+    // Close infile
     fclose(inptr);
+
+    // Close outfile
+    fclose(outptr);
 
     // Success
     return 0;
