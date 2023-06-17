084d568b272ebee3b7c3d1077fe310999a526be7
Hillgrove <github@hillgrove.dk>
2023-06-17 17:20:14 +0000

    Sat, Jun 17, 2023, 7:20 PM +02:00

--- week04/practice/license/license.c
+++ week04/practice/license/license.c
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
     {
         // Replace '\n' with '\0'
         buffer[6] = '\0';
-        printf("Buffer %i: %s\n", idx, buffer);
+        // printf("Buffer %i: %s\n", idx, buffer);
 
         // Save plate number in array
         plates[idx] = buffer;
-        printf("plates[%i]: %s\n", idx, plates[idx]);
+        // printf("plates[%i]: %s\n", idx, plates[idx]);
         idx++;
     }
 
