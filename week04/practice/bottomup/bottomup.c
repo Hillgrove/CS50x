b632c1670dbbfb2f23e9011bcfdb3643e39bdf3b
Hillgrove <github@hillgrove.dk>
2023-06-17 13:03:15 +0000

    Sat, Jun 17, 2023, 3:03 PM +02:00

--- week04/practice/bottomup/bottomup.c
+++ week04/practice/bottomup/bottomup.c
@@ -74,8 +74,8 @@ int main(int argc, char *argv[])
             // Read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // reverse image
-            
+
+
 
             // Write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
