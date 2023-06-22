bb9e03208bc19ff35778926d66b1ec5f4fe3b805
Hillgrove <github@hillgrove.dk>
2023-06-22 20:56:18 +0000

    Thu, Jun 22, 2023, 10:56 PM +02:00

--- week04/lab04/smiley/helpers.c
+++ week04/lab04/smiley/helpers.c
@@ -2,6 +2,21 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    RGBTRIPLE new_color;
+    new_color.rgbtRed = 127;
+    new_color.rgbtGreen = 127;
+    new_color.rgbtBlue = 127;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtRed == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtBlue == 0)
+            {
+                image[i][j] = new_color;
+            }
+        }
+    }
+
     // Change all black pixels to a color of your choosing
 }
