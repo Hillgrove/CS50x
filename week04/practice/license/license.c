bee9381462e877581c28827752b016397dd6bc26
Hillgrove <github@hillgrove.dk>
2023-06-22 13:50:00 +0000

    Thu, Jun 22, 2023, 3:50 PM +02:00

--- week04/practice/license/license.c
+++ week04/practice/license/license.c
@@ -24,10 +24,10 @@ int main(int argc, char *argv[])
         // Replace '\n' with '\0'
         buffer[6] = '\0';
 
-
-
         // Save plate number in array
         // MALLOC STUFF
+        // check for null
+        // remember to free
         plates[idx] = buffer;
         idx++;
     }
