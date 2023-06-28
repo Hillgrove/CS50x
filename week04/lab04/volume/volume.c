f3fce71a2ec510e7c407d3579c99b119e8268d09
Hillgrove <github@hillgrove.dk>
2023-06-28 16:06:32 +0000

    Wed, Jun 28, 2023, 6:06 PM +02:00

--- week04/lab04/volume/volume.c
+++ week04/lab04/volume/volume.c
@@ -37,8 +37,8 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
 
-    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
-    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+    fread(header, sizeof(uint8_t), 1, input);
+    fwrite(header, sizeof(uint8_t), 1, output);
 
 
     // TODO: Read samples from input file and write updated data to output file
