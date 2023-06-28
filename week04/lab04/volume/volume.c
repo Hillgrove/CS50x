2ebc93b4436b4334ea80958503c2459d44eece2f
Hillgrove <github@hillgrove.dk>
2023-06-28 15:56:45 +0000

    Wed, Jun 28, 2023, 5:56 PM +02:00

--- week04/lab04/volume/volume.c
+++ week04/lab04/volume/volume.c
@@ -42,12 +42,12 @@ int main(int argc, char *argv[])
 
 
     // TODO: Read samples from input file and write updated data to output file
-    int16_t *buffer;
+    int16_t buffer;
 
     while(fread(&buffer, sizeof(int16_t), 1, input))
     {
-        buffer = malloc(sizeof(int16_t));
         fread(&buffer, sizeof(int16_t), 1, input);
+        buffer = buffer * factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
