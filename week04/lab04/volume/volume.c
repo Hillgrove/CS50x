164001d1f1cf3aeabd8926d356f2bac88a967318
Hillgrove <github@hillgrove.dk>
2023-06-28 15:16:44 +0000

    Wed, Jun 28, 2023, 5:16 PM +02:00

--- week04/lab04/volume/volume.c
+++ week04/lab04/volume/volume.c
@@ -42,6 +42,10 @@ int main(int argc, char *argv[])
 
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    fread();
+    
+    fwrite();
 
 
     // Close files
