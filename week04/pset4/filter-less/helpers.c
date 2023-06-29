9f53a1d6c42eb6f6b779e8e9d90b6a2d54fccb3c
Hillgrove <github@hillgrove.dk>
2023-06-29 11:46:13 +0000

    Thu, Jun 29, 2023, 1:46 PM +02:00

--- week04/pset4/filter-less/helpers.c
+++ week04/pset4/filter-less/helpers.c
@@ -19,7 +19,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+            int sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+            int sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+        }
+    }
 }
 
 // Reflect image horizontally
