5b5971284c4412fdacb51dbb95842e4067b8018c
Hillgrove <github@hillgrove.dk>
2023-06-29 11:39:05 +0000

    Thu, Jun 29, 2023, 1:39 PM +02:00

--- week04/pset4/filter-less/helpers.c
+++ week04/pset4/filter-less/helpers.c
@@ -7,7 +7,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            int average =
+            image[i][j].rgbtBlue = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
         }
     }
     return;
