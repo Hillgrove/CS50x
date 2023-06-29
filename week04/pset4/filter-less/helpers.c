74aed4944baa3a1d26c4202fdcd22f24696e4cd4
Hillgrove <github@hillgrove.dk>
2023-06-29 18:21:09 +0000

    Thu, Jun 29, 2023, 8:21 PM +02:00

--- week04/pset4/filter-less/helpers.c
+++ week04/pset4/filter-less/helpers.c
@@ -57,5 +57,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+            // copy image to new image
+            // calc average of all 9 pixels, 8 surrounding plus the pixel itself
+        }
+    }
     return;
 }
+
