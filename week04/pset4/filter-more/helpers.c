beda50321c09b3b46f681ce013851cceaa0bf3bc
Hillgrove <github@hillgrove.dk>
2023-07-04 18:39:02 +0000

    Tue, Jul 4, 2023, 8:39 PM +02:00

--- week04/pset4/filter-more/helpers.c
+++ week04/pset4/filter-more/helpers.c
@@ -130,22 +130,13 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
                     else
                     {
-                        for (int m = 0; m < 3; m++)
-                        {
-                            for (int n = 0; n < 3; n++)
-                            {
-                                GxRed += copy[k][l].rgbtRed * Gx[m][n];
-                                GxGreen += copy[k][l].rgbtGreen * Gx[m][n];
-                                GxBlue += copy[k][l].rgbtBlue * Gx[m][n];
+                        GxRed += copy[k][l].rgbtRed * Gx[m][n];
+                        GxGreen += copy[k][l].rgbtGreen * Gx[m][n];
+                        GxBlue += copy[k][l].rgbtBlue * Gx[m][n];
 
-                                GyRed += copy[k][l].rgbtRed * Gy[m][n];
-                                GyGreen += copy[k][l].rgbtGreen * Gy[m][n];
-                                GyBlue += copy[k][l].rgbtBlue * Gy[m][n];
-
-                            }
-                        }
-
-
+                        GyRed += copy[k][l].rgbtRed * Gy[m][n];
+                        GyGreen += copy[k][l].rgbtGreen * Gy[m][n];
+                        GyBlue += copy[k][l].rgbtBlue * Gy[m][n];
                     }
                 }
             }
