6b9b2e91cb9f35877bea5f0dd73b00597b1c41fa
Hillgrove <github@hillgrove.dk>
2023-07-06 11:06:13 +0000

    Thu, Jul 6, 2023, 1:06 PM +02:00

--- week04/pset4/recover/recover.c
+++ week04/pset4/recover/recover.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+bool argument_ok(int argc, char *argv[]);
+
 int main(int argc, char *argv[])
 {
+    if (argument_ok(argc, argv))
+    {
+        return 1;
+    }
     // TODO: Open memory card
     // TODO: Look for beginning of JPEG
     // TODO: Open a new JPEG file
     // TODO: Write 512 bytes until a new JPEG is found
-    // TODO: Stop at end of file<
+    // TODO: Stop at end of file
+}
 
-
+bool argument_ok(int argc, char *argv[])
+{
 
 }
