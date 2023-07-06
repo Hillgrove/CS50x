8c6b74f5f742c05f6dbe60a25297dc1738235692
Hillgrove <github@hillgrove.dk>
2023-07-06 11:22:47 +0000

    Thu, Jul 6, 2023, 1:22 PM +02:00

--- week04/pset4/recover/recover.c
+++ week04/pset4/recover/recover.c
@@ -3,14 +3,11 @@
 #include <stdlib.h>
 
 
-bool wrong_arguments(int argc, char *argv[]);
+int wrong_arguments(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    if (wrong_arguments(argc, argv))
-    {
-        return 1;
-    }
+    wrong_arguments(argc, argv);
     // TODO: Open memory card
     // TODO: Look for beginning of JPEG
     // TODO: Open a new JPEG file
@@ -18,12 +15,12 @@ int main(int argc, char *argv[])
     // TODO: Stop at end of file
 }
 
-bool wrong_arguments(int argc, char *argv[])
+int wrong_arguments(int argc, char *argv[])
 {
     if (argc != 2)
     {
         printf("Usage: ./recover filename\n");
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
