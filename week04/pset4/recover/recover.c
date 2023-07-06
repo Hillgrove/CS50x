a072f3232346bdf67e2a064690376f493e5f2df2
Hillgrove <github@hillgrove.dk>
2023-07-06 09:38:31 +0000

    Thu, Jul 6, 2023, 11:38 AM +02:00

--- week04/pset4/recover/recover.c
+++ week04/pset4/recover/recover.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // TODO: Open memory card
+    // TODO: Look for beginning of JPEG
+    // TODO: Open a new JPEG file
+    // TODO: Write 512 bytes until a new JPEG is found
+
+
+
 }
