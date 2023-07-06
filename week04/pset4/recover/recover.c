dfd003677817a71306aced0de50ce326a8adb33f
Hillgrove <github@hillgrove.dk>
2023-07-06 11:38:08 +0000

    Thu, Jul 6, 2023, 1:38 PM +02:00

--- week04/pset4/recover/recover.c
+++ week04/pset4/recover/recover.c
@@ -7,17 +7,30 @@ bool wrong_arguments(int argc);
 
 int main(int argc, char *argv[])
 {
+    // Check if CLI arguments are correct
     if (wrong_arguments(argc))
     {
         return 1;
     }
 
-    
+    // Remember filename
+    char *infile = argv[1];
+
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 1;
+    }
+
+
+
     // TODO: Open memory card
     // TODO: Look for beginning of JPEG
     // TODO: Open a new JPEG file
     // TODO: Write 512 bytes until a new JPEG is found
     // TODO: Stop at end of file
+    close
 }
 
 bool wrong_arguments(int argc)
