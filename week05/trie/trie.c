d1e85aed1d0a0ad1203999f884a349b13613f381
Hillgrove <github@hillgrove.dk>
2023-07-13 13:59:23 +0000

    Thu, Jul 13, 2023, 3:59 PM +02:00

--- week05/trie/trie.c
+++ week05/trie/trie.c
@@ -110,7 +110,8 @@ int main(int argc, char *argv[])
 // TODO: Complete the check function, return true if found, false if not found
 bool check(char* word)
 {
-    node *cursor = NULL;
+    node *cursor = root;
+    
     for (int i = 0, n = strlen(word); i < n; i++)
     {
         if
