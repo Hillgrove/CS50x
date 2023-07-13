fa65b40b192d2056bbff4b6f58fe3907c191a672
Hillgrove <github@hillgrove.dk>
2023-07-13 14:08:11 +0000

    Thu, Jul 13, 2023, 4:08 PM +02:00

--- week05/trie/trie.c
+++ week05/trie/trie.c
@@ -115,16 +115,12 @@ bool check(char* word)
     for (int i = 0, n = strlen(word); i < n; i++)
     {
         int index = tolower(word[i]) - 'a';
+        cursor = cursor->children[index];
+    }
 
-        if (cursor->is_word == true)
-        {
-            return true;
-        }
-
-        else
-        {
-            cursor
-        }
+    if (cursor->is_word == true)
+    {
+        return true;
     }
     return false;
 }
