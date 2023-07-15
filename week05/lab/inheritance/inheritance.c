16ef2ae4fc6722aafe5a61cd3baeea18b56e9433
Hillgrove <github@hillgrove.dk>
2023-07-15 10:38:48 +0000

    Sat, Jul 15, 2023, 12:38 PM +02:00

--- week05/lab/inheritance/inheritance.c
+++ week05/lab/inheritance/inheritance.c
@@ -50,8 +50,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
 
     }
 
@@ -59,8 +63,12 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
 
     }
 
