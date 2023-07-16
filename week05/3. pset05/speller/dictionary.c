e2e5834998ccde13d0f55a564136a7bca5cb015b
Hillgrove <github@hillgrove.dk>
2023-07-16 22:02:51 +0000

    Mon, Jul 17, 2023, 12:02 AM +02:00

--- week05/3. pset05/speller/dictionary.c
+++ week05/3. pset05/speller/dictionary.c
@@ -22,7 +22,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table - default 26
-const unsigned int N = 26;
+const unsigned int N = 186018;
 unsigned int count = 0;
 
 // Hash table
