4b16de2730dc468b604453cd6a7b62d0cf5f77c4
Hillgrove <github@hillgrove.dk>
2023-07-17 20:22:30 +0000

    Mon, Jul 17, 2023, 10:22 PM +02:00

--- week05/3. pset05/speller/dictionary.c
+++ week05/3. pset05/speller/dictionary.c
@@ -134,7 +134,6 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
     for (int i = 0; i < N; i++)
     {
         while (table[i] != NULL)
