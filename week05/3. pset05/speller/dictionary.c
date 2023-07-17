3dfd42fa400f40a62d5f1748b33dfa096da82d81
Hillgrove <github@hillgrove.dk>
2023-07-17 12:22:55 +0000

    Mon, Jul 17, 2023, 2:22 PM +02:00

--- week05/3. pset05/speller/dictionary.c
+++ week05/3. pset05/speller/dictionary.c
@@ -15,7 +15,7 @@
 #define OFFSET_32 2166136261
 
 // Size of table: Next prime after load factor of 0,7 achieved (204427)
-const unsigned int N = 204427;
+const unsigned int N = 20;
 unsigned int count = 0;
 
 // Represents a node in a hash table
