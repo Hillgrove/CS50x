f5a1fd6c5f5c4b1b13230d8315675a9cea7e6313
Hillgrove <github@hillgrove.dk>
2023-07-18 20:34:09 +0000

    Tue, Jul 18, 2023, 10:34 PM +02:00

--- week06/1. practice/figlet/figlet.py
+++ week06/1. practice/figlet/figlet.py
@@ -3,15 +3,12 @@ from pyfiglet import Figlet
 
 def main():
     text = input("Input: ").strip()
-
     argc = len(text.split())
 
-    print(f"number of args: {argc}")
-
-    if len(text) == 1:
+    if argc == 1:
         ...
 
-    if len(text) == 3:
+    if argc == 3:
         ...
 
 if __name__ == "__main__":
