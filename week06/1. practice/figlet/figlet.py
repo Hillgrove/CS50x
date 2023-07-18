f3b06cba9b5097ea02319a8bb69ddacdd97c9a0d
Hillgrove <github@hillgrove.dk>
2023-07-18 19:55:22 +0000

    Tue, Jul 18, 2023, 9:55 PM +02:00

--- week06/1. practice/figlet/figlet.py
+++ week06/1. practice/figlet/figlet.py
@@ -3,9 +3,13 @@ import sys
 
 
 def main:
-    
+    text = input("Input: ")
 
+    if len(sys.argv) == 1:
+        ...
 
+    if len(sys.argv) == 3:
+        ...
 
 if __name__ == "__main__":
     main()
