2c1dfed997ba0b64b48813f4e6d57340fd39f918
Hillgrove <github@hillgrove.dk>
2023-07-18 20:38:15 +0000

    Tue, Jul 18, 2023, 10:38 PM +02:00

--- week06/1. practice/figlet/figlet.py
+++ week06/1. practice/figlet/figlet.py
@@ -1,12 +1,18 @@
+import random
 from pyfiglet import Figlet
+
 
 
 def main():
     text = input("Input: ").strip().split()
     argc = len(text)
 
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+
+
     if argc == 1:
-        ...
+        
 
     if argc == 3:
         ...
