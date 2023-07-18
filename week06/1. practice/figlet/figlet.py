85416dd1e236189500f49e4ba9907d6228b3b875
Hillgrove <github@hillgrove.dk>
2023-07-18 21:24:24 +0000

    Tue, Jul 18, 2023, 11:24 PM +02:00

--- week06/1. practice/figlet/figlet.py
+++ week06/1. practice/figlet/figlet.py
@@ -4,19 +4,24 @@ from pyfiglet import Figlet
 
 
 def main():
+
+    # Check for correct input
     argc = len(sys.argv) - 1
 
     if argc not in [0, 2]:
         print("Invalid usage")
         sys.exit(1)
 
-    if sys.ar
+    if sys.argv[1] not in ["-f", "--font"] and sys-argv[2] not in fonts:
+        print("Invalid usage")
+        sys.exit(1)
 
-    text = input("Input: ").strip()
-
-
+    # Initialize Figlet module
     figlet = Figlet()
     fonts = figlet.getFonts()
+
+    # Get input from user
+    text = input("Input: ").strip()
 
     if argc == 0:
         print(argc)
@@ -24,14 +29,9 @@ def main():
         figlet.setFont(font=random_font)
         print(f"Output: \n{figlet.renderText(text)}")
 
-    elif argc == 2:
-        print(argc)
-        if sys.argv[1] not in ["-f", "--font"] and sys-argv[2] not in fonts:
-            print("Invalid usage")
-            sys.exit(1)
-        else:
-            figlet.setFont(font=text[1])
-            print(figlet.renderText(text[2]))
+    else argc == 2:
+        figlet.setFont(font=text[1])
+        print(figlet.renderText(text[2]))
 
 
 if __name__ == "__main__":
