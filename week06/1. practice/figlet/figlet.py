a93320c7f93aae830dc160f002bccc84bcb86944
Hillgrove <github@hillgrove.dk>
2023-07-18 20:51:50 +0000

    Tue, Jul 18, 2023, 10:51 PM +02:00

--- week06/1. practice/figlet/figlet.py
+++ week06/1. practice/figlet/figlet.py
@@ -1,6 +1,6 @@
 import random
+import sys
 from pyfiglet import Figlet
-
 
 
 def main():
@@ -17,11 +17,12 @@ def main():
         print("zero argument")
         # print(figlet.renderText(text))
 
-    elif argc == 3 and text[1] in ["-f", "--font"] and text[2] in fonts:
-        print("3 arguments")
-
-    else:
-        
+    elif argc == 3:
+        if text[1] not in ["-f", "--font"] and text[2] not in fonts:
+            print("3 arguments but wrong arguments")
+            sys.exit()
+        else:
+            print("3 arguments and correct arguments")
 
 
 if __name__ == "__main__":
