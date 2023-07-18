b3656d4269bf24b7fb010b4a0099fa03c3160d38
Hillgrove <github@hillgrove.dk>
2023-07-18 19:18:18 +0000

    Tue, Jul 18, 2023, 9:18 PM +02:00

--- week06/1. practice/bank/bank.py
+++ week06/1. practice/bank/bank.py
@@ -1,7 +1,9 @@
+import sys
+
 def main():
     greeting = input("Greeting: ").lower()
 
-    if greeting == "hello":
+    if greeting[1] == "hello" and len(sys.argv) == 2:
         print("$0")
     elif greeting[0] == "h":
         print("$20")
