6ced66a28bc215b98fd63dc2ba01bad5a984128e
Hillgrove <github@hillgrove.dk>
2023-07-18 21:18:19 +0000

    Tue, Jul 18, 2023, 11:18 PM +02:00

--- week06/1. practice/figlet/figlet.py
+++ week06/1. practice/figlet/figlet.py
@@ -4,7 +4,7 @@ from pyfiglet import Figlet
 
 
 def main():
-    print(len(sys.argv))
+    print(len(sys.argv) - 1)
     text = input("Input: ").strip()
     argc = len(sys.argv) - 1
 
@@ -13,11 +13,13 @@ def main():
     fonts = figlet.getFonts()
 
     if argc == 0:
+        print(argc)
         random_font = random.choice(fonts)
         figlet.setFont(font=random_font)
         print(f"Output: \n{figlet.renderText(text)}")
 
     elif argc == 2:
+        print(argc)
         if sys.argv[1] not in ["-f", "--font"] and sys-argv[2] not in fonts:
             print("Invalid usage")
             sys.exit(1)
