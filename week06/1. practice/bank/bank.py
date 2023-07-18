6918c87f36c8c2d351d2c6c90514b2b625150699
Hillgrove <github@hillgrove.dk>
2023-07-18 19:27:11 +0000

    Tue, Jul 18, 2023, 9:27 PM +02:00

--- week06/1. practice/bank/bank.py
+++ week06/1. practice/bank/bank.py
@@ -1,9 +1,9 @@
 def main():
     greeting = input("Greeting: ").lower()
 
-    if greeting.startswith == "hello":
+    if greeting[:5] == "hello":
         print("$0")
-    elif greeting[0] == "h":
+    elif greeting[:1] == "h":
         print("$20")
     else:
         print("$100")
