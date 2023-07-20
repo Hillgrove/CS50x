e70fc839f150cbdab506c91070d4239bd7e50c6a
Hillgrove <github@hillgrove.dk>
2023-07-20 11:32:38 +0000

    Thu, Jul 20, 2023, 1:32 PM +02:00

--- week06/1. practice/seven-day-average/seven-day-average.py
+++ week06/1. practice/seven-day-average/seven-day-average.py
@@ -39,7 +39,7 @@ def calculate(reader):
     new_cases = {}
     for row in reader:
         if row["state"] not in new_cases:
-            new_cases()
+            new_cases("state") = []
 
 
 # TODO: Calculate and print out seven day average for given state
