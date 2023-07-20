a6d71264e5eb1c87b39c2d22b6afe82e5bb000cd
Hillgrove <github@hillgrove.dk>
2023-07-20 12:06:24 +0000

    Thu, Jul 20, 2023, 2:06 PM +02:00

--- week06/1. practice/seven-day-average/seven-day-average.py
+++ week06/1. practice/seven-day-average/seven-day-average.py
@@ -41,7 +41,7 @@ def calculate(reader):
         if row["state"] not in new_cases:
             new_cases[row["state"]] = [int(row["deaths"])]
         else:
-            new_cases[row["state"]].append(int([row["deaths"]]) - new_cases["state"] - 1)
+            new_cases[row["state"]].append(int([row["deaths"]]) - new_cases["state"][- 1])
         if len(new_cases[row["state"]]) > 14:
             new_cases[row["state"]].pop(0)
 
