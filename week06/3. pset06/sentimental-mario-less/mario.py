96c5b4e6905f54a884de8afc73743016a985376b
Hillgrove <github@hillgrove.dk>
2023-07-22 14:17:06 +0000

    Sat, Jul 22, 2023, 4:17 PM +02:00

--- week06/3. pset06/sentimental-mario-less/mario.py
+++ week06/3. pset06/sentimental-mario-less/mario.py
@@ -5,7 +5,7 @@ def main():
             break
 
     for i in range(height):
-        print(f"{(height - i - 1) * ' '}{(i + 1) * '*'}")
+        print(f"{(height - i - 1) * ' '}{(i + 1) * '#'}")
 
 
 if __name__ == "__main__":
