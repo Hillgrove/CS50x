56f5ecbd850b0627690eb2a2df735835466681be
Hillgrove <github@hillgrove.dk>
2023-07-22 12:42:52 +0000

    Sat, Jul 22, 2023, 2:42 PM +02:00

--- week06/2. lab06/world-cup/tournament.py
+++ week06/2. lab06/world-cup/tournament.py
@@ -29,8 +29,7 @@ def main():
     for i in range(N):
         winner = simulate_tournament(teams)
         if winner not in counts:
-            counts.add(winner)
-            counts[winner] += 1
+            counts[winner] = 1
         else:
             counts[winner] += 1
 
