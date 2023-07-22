fee23655c14a7fe82106b64ab9c15f5b29e97b1f
Hillgrove <github@hillgrove.dk>
2023-07-22 14:32:41 +0000

    Sat, Jul 22, 2023, 4:32 PM +02:00

---
+++ week06/3. pset06/sentimental-mario-more/mario.py
@@ -0,0 +1,19 @@
+def main():
+    while True:
+        try:
+            height = int(input("Height: "))
+        except EOFError:
+            print()
+            exit()
+        except:
+            print("Please input an integer between 0 and 9.")
+        else:
+            if 0 < height < 9:
+                break
+
+    for i in range(height):
+        print(f"{(height - i - 1) * ' '}{(i + 1) * '#'} {(i + 1) * '#'}")
+
+
+if __name__ == "__main__":
+    main()
