18e8d2448af21ef10513687ff7182f3b1f9cc52b
Hillgrove <github@hillgrove.dk>
2023-07-22 14:16:24 +0000

    Sat, Jul 22, 2023, 4:16 PM +02:00

--- week06/3. pset06/sentimental-mario-less/mario.py
+++ week06/3. pset06/sentimental-mario-less/mario.py
@@ -1,9 +1,9 @@
 def main():
+    while True:
+        height = int(input("Height: "))
+        if height > 0:
+            break
 
-    height = int(input("Height: "))
-    while height <= 0:
-            height = int(input("Height: "))
-    
     for i in range(height):
         print(f"{(height - i - 1) * ' '}{(i + 1) * '*'}")
 
