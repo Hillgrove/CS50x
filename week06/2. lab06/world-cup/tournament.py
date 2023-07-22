0440819339e985930e955476a7a9184da836a7a1
Hillgrove <github@hillgrove.dk>
2023-07-22 12:26:35 +0000

    Sat, Jul 22, 2023, 2:26 PM +02:00

--- week06/2. lab06/world-cup/tournament.py
+++ week06/2. lab06/world-cup/tournament.py
@@ -65,7 +65,7 @@ def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     while len(teams) > 1:
         teams = simulate_round(teams)
-    return teams
+    return teams["team"]
 
 
 
