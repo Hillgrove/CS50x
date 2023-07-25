3217cb891fcc8ee02cdcc9e9d73399362226fae9
Hillgrove <github@hillgrove.dk>
2023-07-25 17:11:11 +0000

    Tue, Jul 25, 2023, 7:11 PM +02:00

--- my_programs/word_frequency_counter/wordcounter.py
+++ my_programs/word_frequency_counter/wordcounter.py
@@ -7,9 +7,20 @@ and the values are the frequency of each word in the list.
 import sys
 
 def main():
+
+    wordcount = {}
     filename = sys.argv[1]
     with open(filename, "r") as file:
-        
+        for line in file:
+            words = line.split()
+            for word in words:
+                if word not in wordcount:
+                    wordcount[word] = 1
+                else:
+                    wordcount[word] += 1
+
+    for word in wordcount:
+        print(f"{word}: {wordcount[word]}")
 
 
 
