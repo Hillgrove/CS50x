b2120dceb60719bed26bbc939645ce2cea6b3b43
Hillgrove <github@hillgrove.dk>
2023-07-25 17:05:10 +0000

    Tue, Jul 25, 2023, 7:05 PM +02:00

--- my_programs/word_frequency_counter/wordcounter.py
+++ my_programs/word_frequency_counter/wordcounter.py
@@ -4,3 +4,14 @@ Given a list of words, create a dictionary where the keys are the words,
 and the values are the frequency of each word in the list.
 """
 
+import sys
+
+def main():
+    filename = sys.argv[1]
+    with open(filename, "r") as file:
+        
+
+
+
+if __name__ == "__main__":
+    main()
