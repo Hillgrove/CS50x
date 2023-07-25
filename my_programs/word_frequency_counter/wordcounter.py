3e212e5020529ca6705d29005e76e5340d8b46a7
Hillgrove <github@hillgrove.dk>
2023-07-25 17:20:11 +0000

    Tue, Jul 25, 2023, 7:20 PM +02:00

--- my_programs/word_frequency_counter/wordcounter.py
+++ my_programs/word_frequency_counter/wordcounter.py
@@ -4,20 +4,17 @@ Given a list of words, create a dictionary where the keys are the words,
 and the values are the frequency of each word in the list.
 """
 
+from collections import Counter
 import sys
+
 
 def main():
 
     wordcount = {}
     filename = sys.argv[1]
     with open(filename, "r") as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                if word not in wordcount:
-                    wordcount[word] = 1
-                else:
-                    wordcount[word] += 1
+        words = file.read().split()
+        wordcount = Counter(words)
 
     for word in wordcount:
         print(f"{word}: {wordcount[word]}")
