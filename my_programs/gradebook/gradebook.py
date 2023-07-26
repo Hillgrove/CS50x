f90f3f9c96a20b8919077cfed44586f96483859d
Hillgrove <github@hillgrove.dk>
2023-07-26 16:17:28 +0000

    Wed, Jul 26, 2023, 6:17 PM +02:00

--- my_programs/gradebook/gradebook.py
+++ my_programs/gradebook/gradebook.py
@@ -47,10 +47,10 @@ def add_student(students, courses):
             print()
             break
 
+    print(f"If {name} didn't take course, press enter, else type the grade achieved (0 to 10). ?")
     for course in courses:
-        print(f"If {name} didn't take {course}, press enter, else type the grade achieved (0 to 10). ?")
         while True:
-            grade = input("Enter grade: ")
+            grade = input(f"Enter grade for {course}: ")
             try:
                 grade = int(grade)
             except ValueError:
