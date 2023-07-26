d6bad8f974c2b0bd73fc39cb123dd7942615b153
Hillgrove <github@hillgrove.dk>
2023-07-26 13:30:19 +0000

    Wed, Jul 26, 2023, 3:30 PM +02:00

--- my_programs/gradebook/gradebook.py
+++ my_programs/gradebook/gradebook.py
@@ -43,7 +43,7 @@ def add_student():
         if name in students:
             print("Student already in gradebook. Please try again.")
         else:
-            students.append(name)
+            students.append("name"; name)
             break
     print(students)
 
