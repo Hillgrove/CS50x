5ed404999c5386ed73791238748a00b9acce1a32
Hillgrove <github@hillgrove.dk>
2023-08-03 10:40:37 +0000

    Thu, Aug 3, 2023, 12:40 PM +02:00

--- week07/3. pset07/movies/11.sql
+++ week07/3. pset07/movies/11.sql
@@ -2,8 +2,10 @@
 -- Your query should output a table with a single column for the title of each movie.
 -- You may assume that there is only one person in the database with the name Chadwick Boseman.
 
-SELECT title
+SELECT title, rating
 FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 JOIN ratings ON movies.id = ratings.movie_id
+WHERE people.name LIKE "Chadwick Boseman"
+ORDER BY ratings.rating DESC
