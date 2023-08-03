fd72a166576e2471c3b47288f46e5b39edaa2422
Hillgrove <github@hillgrove.dk>
2023-08-03 14:58:29 +0000

    Thu, Aug 3, 2023, 4:58 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -22,7 +22,10 @@ Knowledge gained:
 */
 
 -- Finds names of all who made a withdrawal the given day and location
-SELECT DISTINCT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+SELECT DISTINCT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+
+SELECT name
+
 +---------+
 |  name   |
 +---------+
