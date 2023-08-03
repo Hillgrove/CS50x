6389f44109523c95c369aee89025d9cfd90ffada
Hillgrove <github@hillgrove.dk>
2023-08-03 08:36:31 +0000

    Thu, Aug 3, 2023, 10:36 AM +02:00

--- week07/2. lab07/songs/6.sql
+++ week07/2. lab07/songs/6.sql
@@ -1,1 +1,4 @@
 SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name LIKE "Post Malone");
+
+
+SELECT songs.name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name LIKE "Post Malone";
