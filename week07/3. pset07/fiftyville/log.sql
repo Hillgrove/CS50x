ae90b81833bf28d0c5e648ddc6c8542f7b47205c
Hillgrove <github@hillgrove.dk>
2023-08-03 14:56:11 +0000

    Thu, Aug 3, 2023, 4:56 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -54,4 +54,3 @@ SELECT people.name FROM people JOIN bakery_security_logs ON people.license_plate
 +---------+
 
 
-SELECT DISTINCT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw" INTERSECT SELECT people.name FROM people JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate WHERE year =2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = "exit";
