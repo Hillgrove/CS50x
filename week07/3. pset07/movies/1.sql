f82c537c681565c12c05d9d9538a045662b157b7
Hillgrove <github@hillgrove.dk>
2023-08-03 09:40:08 +0000

    Thu, Aug 3, 2023, 11:40 AM +02:00

--- week07/3. pset07/movies/1.sql
+++ week07/3. pset07/movies/1.sql
@@ -1,4 +1,4 @@
 -- Write a SQL query to list the titles of all movies released in 2008.
 -- Your query should output a table with a single column for the title of each movie.
 
-SELECT title, year FROM movies WHERE year = 2008;
+SELECT title FROM movies WHERE year = 2008;
