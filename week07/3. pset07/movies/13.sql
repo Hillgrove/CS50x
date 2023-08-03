0688739e57e000b73d55c6d088355a5d0d026412
Hillgrove <github@hillgrove.dk>
2023-08-03 12:49:52 +0000

    Thu, Aug 3, 2023, 2:49 PM +02:00

--- week07/3. pset07/movies/13.sql
+++ week07/3. pset07/movies/13.sql
@@ -3,17 +3,6 @@
 -- There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
 -- Kevin Bacon himself should not be included in the resulting list.
 
--- SELECT name
--- FROM people
--- JOIN stars ON people.id = stars.person_id
--- WHERE stars.movie_id = (
---     SELECT movies.id
---     FROM movies
---     JOIN stars ON movies.id = stars.movie_id
---     JOIN people ON stars.person_id = people.id
---     WHERE people.name LIKE "Kevin Bacon" AND people.birth = 1958
--- )
-
 -- SELECT movies.id
 -- FROM movies
 -- JOIN stars ON movies.id = stars.movie_id
