9379ee6fbbd2f1fa38bc4c79ab083297e8231d6a
Hillgrove <github@hillgrove.dk>
2023-08-03 13:38:21 +0000

    Thu, Aug 3, 2023, 3:38 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -2,3 +2,4 @@
 
 -- Clues adress and time
 SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street LIKE "Humphrey Street";
+-- Knowledge gained: reportID is 295, theft done at 10:15am, 3 witnesses, all mentions bakery
