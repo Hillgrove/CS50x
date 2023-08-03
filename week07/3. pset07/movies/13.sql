9b3dc994525baea1e00dbcfeefc563039d5bf6e7
Hillgrove <github@hillgrove.dk>
2023-08-03 12:51:35 +0000

    Thu, Aug 3, 2023, 2:51 PM +02:00

--- week07/3. pset07/movies/13.sql
+++ week07/3. pset07/movies/13.sql
@@ -9,10 +9,10 @@
 -- JOIN people ON stars.person_id = people.id
 -- WHERE people.name LIKE "Kevin Bacon" AND people.birth = 1958
 
-SELECT COUNT(DISTINCT(name))
+SELECT DISTINCT(name)
 FROM people
 JOIN stars ON people.id = stars.person_id
-WHERE stars.movie_id IN (
+WHERE people.name NOT LIKE "Kevin Bacon" AND stars.movie_id IN (
     SELECT movies.id
     FROM movies
     JOIN stars ON movies.id = stars.movie_id
