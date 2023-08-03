22245161024df450650b6eb0817df083961e424c
Hillgrove <github@hillgrove.dk>
2023-08-03 09:55:55 +0000

    Thu, Aug 3, 2023, 11:55 AM +02:00

--- week07/3. pset07/movies/8.sql
+++ week07/3. pset07/movies/8.sql
@@ -1,3 +1,5 @@
 -- Write a SQL query to list the names of all people who starred in Toy Story.
 -- Your query should output a table with a single column for the name of each person.
 -- You may assume that there is only one movie in the database with the title Toy Story.
+
+SELECT name FROM movies JOIN people ON movies.id = 
