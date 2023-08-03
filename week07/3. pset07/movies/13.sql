a0e6f9988bbae9ab554cfc2fa7abe399b5fbb37a
Hillgrove <github@hillgrove.dk>
2023-08-03 12:42:45 +0000

    Thu, Aug 3, 2023, 2:42 PM +02:00

--- week07/3. pset07/movies/13.sql
+++ week07/3. pset07/movies/13.sql
@@ -14,8 +14,18 @@
 --     WHERE people.name LIKE "Kevin Bacon" AND people.birth = 1958
 -- )
 
-SELECT movies.id
-FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE people.name LIKE "Kevin Bacon" AND people.birth = 1958
+-- SELECT movies.id
+-- FROM movies
+-- JOIN stars ON movies.id = stars.movie_id
+-- JOIN people ON stars.person_id = people.id
+-- WHERE people.name LIKE "Kevin Bacon" AND people.birth = 1958
+
+SELECT name
+FROM people
+JOIN stars ON people.id = stars.person_id
+WHERE stars.movie_id = (
+    SELECT movies.id
+    FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
+    WHERE people.name LIKE "Kevin Bacon" AND people.birth = 1958)
