c494a9c9a13f610c7f7b3779c6354754210e22e1
Hillgrove <github@hillgrove.dk>
2023-08-03 15:41:55 +0000

    Thu, Aug 3, 2023, 5:41 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -1,46 +1,55 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
 -- Finds crimereports
-SELECT *
-FROM crime_scene_reports
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND street = "Humphrey Street";
-/*
-Knowledge gained:
-    reportID is 295
-    theft done at 10:15am
-    3 witnesses
-    all reports mentions bakery
- */
+SELECT
+  *
+FROM
+  crime_scene_reports
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND street = "Humphrey Street";
 
--- Finds interviews mentioning "bakery"
-SELECT *
-FROM interviews
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND transcript LIKE "%bakery%";
 /*
 Knowledge gained:
-    within 10 mins of theft, thief drove away from parking lot
-    Thief withdrew money from ATM on leggett Street earlier same morning
-    Thief called someone for less than a minute while leaving the bakery
-    Thief planning to take earliest flight out of Fiftyville tomorrow
-    Person on the other end to purchase ticket
+reportID is 295
+theft done at 10:15am
+3 witnesses
+all reports mentions bakery
 */
+-- Finds interviews mentioning "bakery"
+SELECT
+  *
+FROM
+  interviews
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND transcript LIKE "%bakery%";
 
+/*
+Knowledge gained:
+within 10 mins of theft, thief drove away from parking lot
+Thief withdrew money from ATM on leggett Street earlier same morning
+Thief called someone for less than a minute while leaving the bakery
+Thief planning to take earliest flight out of Fiftyville tomorrow
+Person on the other end to purchase ticket
+*/
 -- Finds names of all who made a withdrawal the given day and location
-SELECT name
-FROM people
-JOIN bank_accounts ON people.id = bank_accounts.person_id
-JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND atm_location = "Leggett Street"
-AND transaction_type = "withdraw";
+SELECT
+  name
+FROM
+  people
+  JOIN bank_accounts ON people.id = bank_accounts.person_id
+  JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND atm_location = "Leggett Street"
+  AND transaction_type = "withdraw";
+
 /*
 +---------+
 |  name   |
@@ -55,19 +64,21 @@ AND transaction_type = "withdraw";
 | Benista |
 +---------+
 */
-
 -- Finds the names of all who owns a car that exited the parkinglot between 10:15 and 10:25
-SELECT people.name
-FROM people
-JOIN bakery_security_logs
-ON people.license_plate = bakery_security_logs.license_plate
-WHERE year =2021
-AND month = 7
-AND day = 28
-AND hour = 10
-AND minute > 15
-AND minute < 25
-AND activity = "exit";
+SELECT
+  people.name
+FROM
+  people
+  JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND HOUR = 10
+  AND MINUTE > 15
+  AND MINUTE < 25
+  AND activity = "exit";
+
 /*
 +---------+
 |  name   |
@@ -82,60 +93,65 @@ AND activity = "exit";
 | Kelsey  |
 +---------+
 */
-
 /* Finds the names of those who:
-    - have a car that existed the parkinglot between 10:15 and 10:25
-    - made made a withdrawal the given day and location
-    - made a phonecall less than 60 seconds on the given day
- */
-SELECT name
-FROM phone_calls
-JOIN people ON phone_calls.caller = people.phone_number
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND duration < 60
-
-/*
-+-------+
-| name  |
-+-------+
-| Bruce |
-| Diana |
-+-------+
+- have a car that existed the parkinglot between 10:15 and 10:25
+- made made a withdrawal the given day and location
+- made a phonecall less than 60 seconds on the given day
 */
-SELECT name
-FROM people
-JOIN bank_accounts ON people.id = bank_accounts.person_id
-JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND atm_location = "Leggett Street"
-AND transaction_type = "withdraw"
-
+SELECT
+  name
+FROM
+  phone_calls
+  JOIN people ON phone_calls.caller = people.phone_number
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND duration < 60
+  /*
+  +-------+
+  | name  |
+  +-------+
+  | Bruce |
+  | Diana |
+  +-------+
+  */
+SELECT
+  name
+FROM
+  people
+  JOIN bank_accounts ON people.id = bank_accounts.person_id
+  JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND atm_location = "Leggett Street"
+  AND transaction_type = "withdraw"
 INTERSECT
-
-SELECT people.name
-FROM people
-JOIN bakery_security_logs
-ON people.license_plate = bakery_security_logs.license_plate
-WHERE year =2021
-AND month = 7
-AND day = 28
-AND hour = 10
-AND minute > 15
-AND minute < 25
-AND activity = "exit"
-
+SELECT
+  people.name
+FROM
+  people
+  JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND HOUR = 10
+  AND MINUTE > 15
+  AND MINUTE < 25
+  AND activity = "exit"
 INTERSECT
-
-SELECT name
-FROM people
-JOIN bank_accounts ON people.id = bank_accounts.person_id
-JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND atm_location = "Leggett Street"
-AND transaction_type = "withdraw"
+SELECT
+  name
+FROM
+  people
+  JOIN bank_accounts ON people.id = bank_accounts.person_id
+  JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28
+  AND atm_location = "Leggett Street"
+  AND transaction_type = "withdraw"
