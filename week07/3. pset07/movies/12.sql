912280887beb7ed3cf5dc3b0d2e0412877b74eea
Hillgrove <github@hillgrove.dk>
2023-08-03 10:47:43 +0000

    Thu, Aug 3, 2023, 12:47 PM +02:00

--- week07/3. pset07/movies/12.sql
+++ week07/3. pset07/movies/12.sql
@@ -6,5 +6,6 @@
 SELECT title
 FROM movies
 JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE name LIKE "Bradley Cooper" AND name LIKE "Jennifer Lawrence";
+JOIN people p1 ON stars.person_id = people.id
+JOIN people p2 ON stars.person_id = people.id
+WHERE p1.name LIKE "Bradley Cooper" AND p2.name LIKE "Jennifer Lawrence";
