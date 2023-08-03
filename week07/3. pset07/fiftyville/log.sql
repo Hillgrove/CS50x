b06d3a40d66e7f24fc5e380ad364f16928df0051
Hillgrove <github@hillgrove.dk>
2023-08-03 14:19:44 +0000

    Thu, Aug 3, 2023, 4:19 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -22,11 +22,15 @@ Knowledge gained:
 */
 
 -- Finds all withdrawals the given day and location
-SELECT DISTINCT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+SELECT DISTINCT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+
 /*
 Knowledge gained:
     list of account numbers and amount withdrawn
 */
+
+
+
 
 -- Finds all license plates that exited the parkinglot between 10:15 and 10:25
 SELECT license_plate FROM bakery_security_logs WHERE year =2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = "exit";/*
