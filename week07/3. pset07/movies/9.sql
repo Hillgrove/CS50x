c5c5585169d90867091aed429d77fa09e179e07c
Hillgrove <github@hillgrove.dk>
2023-08-03 10:10:08 +0000

    Thu, Aug 3, 2023, 12:10 PM +02:00

--- week07/3. pset07/movies/9.sql
+++ week07/3. pset07/movies/9.sql
@@ -4,7 +4,7 @@
 -- No need to worry about people who have no birth year listed, so long as those who do have a birth year are listed in order.
 -- If a person appeared in more than one movie in 2004, they should only appear in your results once.
 
-SELECT name
+SELECT people.name, people.birth, movies.year
 FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
