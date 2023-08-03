b7b6c0f17f7de535aed7187e93bc01e7e5460466
Hillgrove <github@hillgrove.dk>
2023-08-03 13:34:22 +0000

    Thu, Aug 3, 2023, 3:34 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -1,5 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-
-
-
+-- Clues adress and time
+SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street LIKE "Humphrey Street";
