c71cf8632cc842614430c2f5c1137592eb225c14
Hillgrove <github@hillgrove.dk>
2023-08-03 13:51:11 +0000

    Thu, Aug 3, 2023, 3:51 PM +02:00

--- week07/3. pset07/fiftyville/log.sql
+++ week07/3. pset07/fiftyville/log.sql
@@ -2,4 +2,17 @@
 
 -- Clues adress and time
 SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street LIKE "Humphrey Street";
--- Knowledge gained: reportID is 295, theft done at 10:15am, 3 witnesses, all mentions bakery
+/*
+Knowledge gained:
+    reportID is 295
+    theft done at 10:15am
+    3 witnesses
+    all reports mentions bakery
+ */
+
+SELECT * from interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
+/*
+Knowledge gained:
+    within 10 mis of theft, thief drove away from parking lot
+    Thief 
+*/
