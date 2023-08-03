6d2c1e42d95e48ddfbdf3c093845658c479d5403
Hillgrove <github@hillgrove.dk>
2023-08-03 10:38:08 +0000

    Thu, Aug 3, 2023, 12:38 PM +02:00

--- week07/3. pset07/movies/10.sql
+++ week07/3. pset07/movies/10.sql
@@ -2,9 +2,8 @@
 -- Your query should output a table with a single column for the name of each person.
 -- If a person directed more than one movie that received a rating of at least 9.0, they should only appear in your results once.
 
-SELECT name, rating
+SELECT DISTINCT name
 FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN ratings ON directors.movie_id = ratings.movie_id
 WHERE rating > 9.0
-ORDER BY name, rating
