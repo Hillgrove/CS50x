935a85f24fbec1cf6623f16a60412fdb42a178a6
Hillgrove <github@hillgrove.dk>
2023-08-28 12:41:30 +0000

    Mon, Aug 28, 2023, 2:41 PM +02:00

--- week09/3. lab09/birthdays/schema.sql
+++ week09/3. lab09/birthdays/schema.sql
@@ -4,3 +4,14 @@ CREATE TABLE birthdays (
     month INTEGER,
     day INTEGER,
     PRIMARY KEY(id)
+);
+
+/*
++----+----------+-------+-----+
+| id |   name   | month | day |
++----+----------+-------+-----+
+| 1  | Harry    | 7     | 31  |
+| 2  | Ron      | 3     | 1   |
+| 3  | Hermione | 9     | 19  |
++----+----------+-------+-----+
+*/
