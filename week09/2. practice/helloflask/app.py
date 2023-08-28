40dc605400cfd31c3d8ec6b59db5ab5934145ca6
Hillgrove <github@hillgrove.dk>
2023-08-28 12:15:57 +0000

    Mon, Aug 28, 2023, 2:15 PM +02:00

--- week09/2. practice/helloflask/app.py
+++ week09/2. practice/helloflask/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-COLORS = ["red", "blue"]
+COLORS = [
+    {"red" = "}
+    , "blue"]
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html", colors=COLORS)
+        return render_template("index.html", )
     else:
         print("Form submitted!")
         color = request.form.get("color")
