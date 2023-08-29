874b71c3e2696077d7cafb2ce79a662682c187f5
Hillgrove <github@hillgrove.dk>
2023-08-29 14:19:18 +0000

    Tue, Aug 29, 2023, 4:19 PM +02:00

--- week09/4.pset09/finance/app.py
+++ week09/4.pset09/finance/app.py
@@ -134,7 +134,7 @@ def register():
 
         # Create user in database
         else:
-            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), request.form.get("password"))
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
 
         return redirect("/")
 
