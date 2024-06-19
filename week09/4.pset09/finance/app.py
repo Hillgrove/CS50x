4e66b15b4278b8058ed567f8e61ec61732c11b86
Hillgrove <github@hillgrove.dk>
2024-06-19 21:14:09 +0000

    Wed, Jun 19, 2024, 11:14 PM +02:00

--- week09/4.pset09/finance/app.py
+++ week09/4.pset09/finance/app.py
@@ -149,7 +149,17 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    history = db.execute("""
+                         SELECT *
+                         FROM history
+                         WHERE user_id = ?
+                         ORDER BY id""", session['user_id'])
+
+    print(f"""== DEBUG ==\n
+              {history}\n
+              == DEBUG ==""")
+    
+    return render_template("history.html", history=history)
 
 
 @app.route("/login", methods=["GET", "POST"])
